5a17509cf58aaca47e0ac924ce52f8deb6363b8f
SpencerLeBard <slebard@gmail.com>
2021-01-22 15:29:32 -0700

    get and getbyid

--- src/Products/products.controller.ts
+++ src/Products/products.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Post , Body} from '@nestjs/common'
+import {Controller, Post , Body, Get, Param} from '@nestjs/common'
 import {ProductsService} from './products.service';
 
 @Controller('products')
@@ -17,4 +17,12 @@ export class ProductsController {
        prodPrice);
       return {id: generatedId}
   }
+  @Get()
+  getAllProducts(){
+      return this.productsService.getProducts();
+  }
+  @Get(':id')
+    getProducts(@Param('id') prodId: string){
+    return this.productsService.getSingleProduct(prodId);
+    }
 }
--- src/Products/products.service.ts
+++ src/Products/products.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common'
+import {Injectable , NotFoundException} from '@nestjs/common'
 import {Product} from './products.model';
 
 
@@ -7,9 +7,21 @@ export class ProductsService{
   products: Product[] = [];
 
   insertProduct(title: string , desc: string , price: number){
-    const prodId = new Date().toString();
+    const prodId = Math.random().toString();
     const newProduct = new Product(prodId, title , desc , price);
     this.products.push(newProduct);
     return prodId
+  }
+
+  getProducts(){
+    //NOTE '...' spread operation - pulling all elements off products, adding to new array
+    return [...this.products]
+  }
+  getSingleProduct(productId: string){
+    const product = this.products.find(prod => prod.id == productId);
+    if(!product){
+      throw new NotFoundException('Could not find product, sorry');
+    }
+    return {...product}
   }
 }
