a76913585ae97154e711df9dd54a907fd94227dd
SpencerLeBard <slebard@gmail.com>
2021-01-22 11:04:44 -0700

    products

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { domain } from 'process';
 import { AppService } from './app.service';
 
+// NOTE your-domain.com/products
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //NOTE argument would be Get('products)
   @Get()
   getHello(): string {
     return this.appService.getHello();
