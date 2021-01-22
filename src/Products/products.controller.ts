21c1298949c27268f5a97e7c42f8417c3345cccf
SpencerLeBard <slebard@gmail.com>
2021-01-22 11:39:48 -0700

    api almost done

--- src/Products/products.controller.ts
+++ src/Products/products.controller.ts
@@ -1,9 +1,16 @@
-import {Controller, Post} from '@nestjs/common'
+import {Controller, Post , Body} from '@nestjs/common'
+import {ProductsService} from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(productsService: ProductsService){
+  }
+
   @Post()
-  addProducts(): any {
-    
+  addProducts(
+  @Body('title') prodTitle: string , 
+  @Body('desciption') prodDesc: string , 
+  @Body('price') prodPrice: number): any {
+      this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
   }
 }
